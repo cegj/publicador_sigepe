391b8c00ef0ca6397908d50464018a25b681d4b0
cegj <cegasparjr@gmail.com>
2023-04-09 17:41:42 -0300

    Corrige find_element_by_xpath

--- src/helpers/checkExistsByXpath.py
+++ src/helpers/checkExistsByXpath.py
@@ -1,9 +1,10 @@
 from selenium.common.exceptions import NoSuchElementException
 from Webdriver import nav
+from selenium.webdriver.common.by import By
 
 def checkExistsByXpath(xpath):
     try:
-        nav.find_element_by_xpath(xpath)
+        nav.find_element(By.XPATH, xpath)
     except NoSuchElementException:
         return False
     return True
