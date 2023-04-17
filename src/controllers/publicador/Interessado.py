79c7949b1dc90e1f7a0312368f10daddfe4ba1d7
cegj <cegasparjr@gmail.com>
2023-04-17 11:15:21 -0300

    Interessado funcionando

--- src/controllers/publicador/Interessado.py
+++ src/controllers/publicador/Interessado.py
@@ -14,7 +14,6 @@ class Interessado:
       sigepe_botaoIncluirInteressado = wait["regular"].until(EC.element_to_be_clickable(
           (By.XPATH, xpaths["publicacao"]["incluirInteressadoBotao"])))
       sigepe_botaoIncluirInteressado.click()
-
       wfl.waitForLoading()
 
       sigepe_campoMatricula = wait["regular"].until(EC.element_to_be_clickable(
@@ -23,10 +22,6 @@ class Interessado:
       time.sleep(0.2)
       sigepe_campoMatricula.send_keys(data)
       sigepe_campoMatricula.send_keys(Keys.ENTER)
-
-      # sigepe_botaoPesquisarInteressado = wait["regular"].until(EC.element_to_be_clickable(
-      #     (By.XPATH, xpaths["publicacao"]["pesquisarInteressadoBotao"])))
-      # sigepe_botaoPesquisarInteressado.click()
       wfl.waitForLoading()
 
       sigepe_checkboxSelecionarServidor = wait["regular"].until(EC.element_to_be_clickable(
@@ -37,24 +32,12 @@ class Interessado:
 
       sigepe_botaoIncluirNaLista = wait["regular"].until(EC.element_to_be_clickable(
         (By.XPATH, xpaths["publicacao"]["incluirInteressadoNaListaBotao"])))
-      sigepe_botaoIncluirNaLista.click()
-
-      # navegador.execute_script(
-      #     "botaoIncluirServidor = document.getElementById('frmCadastrarAto:cadastradorDeAtoParaPublicacao:seletorInteressado:j_idt521:j_idt547');")
-
-      # navegador.execute_script("botaoIncluirServidor.click();")
-
+      nav.execute_script("arguments[0].click();", sigepe_botaoIncluirNaLista);
       wfl.waitForLoading()
 
       sigepe_botaoSelecionarServidor = wait["regular"].until(EC.element_to_be_clickable(
         (By.XPATH, xpaths["publicacao"]["selecionarServidorBotao"])))
-      sigepe_botaoSelecionarServidor.click()
-
-      # navegador.execute_script(
-      #     "botaoSelecionarServidor = document.getElementById('frmCadastrarAto:cadastradorDeAtoParaPublicacao:seletorInteressado:j_idt569');")
-
-      # navegador.execute_script("botaoSelecionarServidor.click();")
-
+      nav.execute_script("arguments[0].click();", sigepe_botaoSelecionarServidor);
       wfl.waitForLoading()
 
       interessadoSelecionado = wait["regular"].until(EC.presence_of_element_located(
