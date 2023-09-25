69794b4e169efa13eff5f1b4be5245467f9fb4bc
cegj <cegasparjr@gmail.com>
2023-09-25 16:01:58 -0300

    FIX: update xpaths

--- src/compile.py
+++ src/compile.py
@@ -1,13 +1,13 @@
 import PyInstaller.__main__
 
-# PyInstaller.__main__.run([
-#     'app.py',
-#     # '--onefile',
-#     '--noconsole',
-#     '--clean',
-#     '--icon=../static/ico.ico',
-#     '--name=publicador_sigepe',
-# ])
+PyInstaller.__main__.run([
+    'app.py',
+    # '--onefile',
+    '--noconsole',
+    '--clean',
+    '--icon=../static/ico.ico',
+    '--name=publicador_sigepe',
+])
 
 PyInstaller.__main__.run([
     'fixer/fixer.py',
