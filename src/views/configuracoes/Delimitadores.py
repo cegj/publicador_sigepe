7bc00af910d78b92ccc8c1ff42fba4542c6c39a0
cegj <cegasparjr@gmail.com>
2023-09-26 11:41:19 -0300

    Atualiza layout da janela de delimitadores

--- src/views/configuracoes/Delimitadores.py
+++ src/views/configuracoes/Delimitadores.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 from views import Interfaces as i
 from models import UserConfig as uc
@@ -10,13 +11,22 @@ class Delimitadores:
       self.delimiters = copy(uc.UserConfig.obterDelimitadoresSalvos())
       self.master = i.Interfaces.novaJanela()
       self.delimitadoresContainer = Frame(self.master)
-      self.delimitadoresContainer.pack()
+      self.delimitadoresContainer.pack(pady="5")
       self.janelaDelimitadores()
 
   def janelaDelimitadores(self):
     self.numero_documento()
     self.matricula_siape()
     self.salvar_delimitadores()
+    infoLabel = ttk.Label(
+      self.delimitadoresContainer,
+      text="Os delimitadores são os termos que se encontram\nimediatamente antes e depois das informações que\nprecisam ser obtidas pelo Publicador Sigepe no\nconteúdo dos documentos a serem publicados.",
+      background="#fff9d9",
+      foreground="#85701d",
+      padding=4,
+      justify=CENTER)
+    infoLabel.pack(pady="5")
+
 
   def numero_documento(self):
     try:
@@ -25,33 +35,52 @@ class Delimitadores:
       def setAfterValue(a=None, b=None, c=None):
         self.delimiters["numero_documento"][1] = afterValue.get()
       container = Frame(self.delimitadoresContainer)
-      container.grid(row=1, column=1, sticky='w')
+      container.pack(pady="5")
       label = Label(
         container,
-        text="Número do documento:",
+        text="Número do documento",
         font=ac.AppConfig.fontes["normal"]
         )
-      label.grid(column=1, row=0, padx=10, pady=5, sticky='w')
+      label.pack()
+
+      fieldsContainer = Frame(container)
+      fieldsContainer.pack(pady="2")
+
+      beforeLabel = Label(
+          fieldsContainer,
+          text="Antes: ",
+          font=ac.AppConfig.fontes["normal"]
+          )
+      beforeLabel.grid(row=1, column=1, sticky="W")
+
       beforeValue = StringVar()
       beforeValue.trace_add("write", setBeforeValue)
       beforeInput = Entry(
-        container,
-        width=20,
+        fieldsContainer,
+        width=22,
         textvariable=beforeValue,
         font=ac.AppConfig.fontes["normal"]
         )
       beforeValue.set(self.delimiters["numero_documento"][0])
-      beforeInput.grid(column=2, row=0)
+      beforeInput.grid(row=1, column=2, sticky="W")
+
+      afterLabel = Label(
+          fieldsContainer,
+          text="Depois: ",
+          font=ac.AppConfig.fontes["normal"]
+          )
+      afterLabel.grid(row=2, column=1, sticky="W")
+
       afterValue = StringVar()
       afterValue.trace_add("write", setAfterValue)
       afterInput = Entry(
-        container,
-        width=20,
+        fieldsContainer,
+        width=22,
         textvariable=afterValue,
         font=ac.AppConfig.fontes["normal"]
         )
       afterValue.set(self.delimiters["numero_documento"][1])
-      afterInput.grid(column=3, row=0)
+      afterInput.grid(row=2, column=2, sticky="W")
     except Exception as e:
       messagebox.showerror("Erro em Delimitadores", e)
       self.master.destroy()
@@ -63,38 +92,56 @@ class Delimitadores:
       def setAfterValue(a=None, b=None, c=None):
         self.delimiters["matricula_siape"][1] = afterValue.get()
       container = Frame(self.delimitadoresContainer)
-      container.grid(row=3, column=1, sticky='w')
+      container.pack(pady="5")
       label = Label(
         container,
-        text="Matrícula SIAPE:",
+        text="Matrícula SIAPE",
         font=ac.AppConfig.fontes["normal"]
         )
-      label.grid(column=1, row=0, padx=10, pady=5, sticky='w')
+      label.pack()
+
+      fieldsContainer = Frame(container)
+      fieldsContainer.pack(pady="2")
+
+      beforeLabel = Label(
+          fieldsContainer,
+          text="Antes: ",
+          font=ac.AppConfig.fontes["normal"]
+          )
+      beforeLabel.grid(row=1, column=1, sticky="W")
+
       beforeValue = StringVar()
       beforeValue.trace_add("write", setBeforeValue)
       beforeInput = Entry(
-        container,
+        fieldsContainer,
         width=20,
         textvariable=beforeValue,
         font=ac.AppConfig.fontes["normal"]
         )
       beforeValue.set(self.delimiters["matricula_siape"][0])
-      beforeInput.grid(column=2, row=0)
+      beforeInput.grid(row=1, column=2, sticky="W")
+
+      afterLabel = Label(
+          fieldsContainer,
+          text="Depois: ",
+          font=ac.AppConfig.fontes["normal"]
+          )
+      afterLabel.grid(row=2, column=1, sticky="W")
+
       afterValue = StringVar()
       afterValue.trace_add("write", setAfterValue)
       afterInput = Entry(
-        container,
+        fieldsContainer,
         width=20,
         textvariable=afterValue,
         font=ac.AppConfig.fontes["normal"]
         )
       afterValue.set(self.delimiters["matricula_siape"][1])
-      afterInput.grid(column=3, row=0)
+      afterInput.grid(row=2, column=2, sticky="W")
 
     except Exception as e:
       messagebox.showerror("Erro em Delimitadores", e)
       self.master.destroy()
-
 
   def salvar_delimitadores(self):
     def salvar():
@@ -105,7 +152,7 @@ class Delimitadores:
         messagebox.showerror("Erro ao gravar delimitadores", e)
 
     container = Frame(self.delimitadoresContainer)
-    container.grid(row=4, column=1, sticky='w')
+    container.pack(pady="5")
     salvarDelimitadoresBtn = Button(
       container,
       text="Salvar",
@@ -113,4 +160,4 @@ class Delimitadores:
       width=20,
       command=salvar
     )
-    salvarDelimitadoresBtn.grid(column=1, row=0, padx=10, pady=5, sticky='w')
+    salvarDelimitadoresBtn.pack(pady="5")
